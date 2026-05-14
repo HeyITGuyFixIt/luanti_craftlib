a04830666dcbcae1d67b8408ca53d3463acd93ea
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2026-05-14 10:18:48 -0400

    Reworking knapping to work with craftlib

--- craftlib/README.md
+++ craftlib/README.md
@@ -22,7 +22,7 @@ You can set the `base` to a list of any node or group that can be crafted on. Fo
 
 You can set the `input` to a list of any item, node, or group that the player can craft with. This follows the same logic as a shapeless recipe. The player must sneak and place in the same spot to trigger this.
 
-You can set the `tool` to any tool or group to specify what tool the player crafts with. The player can dig with this tool to trigger this action.
+You can set the `tool` to any item, tool, or group to specify what tool the player crafts with. The player can dig with this tool to trigger this action. If the tool is an item, it is consumed. If it is a tool, it receives damage based on its `tool_capabilities`.
 
 You can set the `output` to a list of items that are created from this craft. Instead of replacements, like in craft recipes, specify the output of a replacement here. So if the `input` includes a bucket of water, add an empty bucket to the `output`, alongside the regular output of the craft.
 
@@ -56,13 +56,16 @@ craftlib.register_craft({
         {0, 0, 0, 0, 0, 1, 1, 1},
         {0, 0, 0, 0, 0, 0, 1, 1}
     },
-    output = {"knapping:pick_head_stone"}
+    output = {"knapping:pick_head_stone"},
+    texture = "default_stone.png"
 })
 ```
 
 This is a unique type of craft called knapping. The idea is you strike two materials together in an orderly way, and you can make toolheads from that. Due to the uniqueness of this method, it has a slightly different set of parameters.
 
 * `pattern`: This is a list of lists, each containing a series of 1s and 0s. This creates an 8x8 grid that the player has to tap to remove chunks from the input material.
+
+* `texture`: This is the texture displayed on the entity while knapping. If not specified, it uses the texture of the base item.
 
 ### Tool Repair
 
--- knapping_mtg/init.lua
+++ knapping_mtg/init.lua
@@ -1,127 +1,127 @@
-knapping_mtg = {}
-local tools = {
-    pick = {
-        title = 'Pickaxe',
-        groups = { pickaxe = 1 },
-    },
-    axe = {
-        title = 'Axe',
-        groups = { axe = 1 },
-    },
-    shovel = {
-        title = 'Shovel',
-        groups = { shovel = 1 },
-    },
-    sword = {
-        title = 'Sword',
-        groups = { sword = 1 },
-    },
-}
-
-local materials = {
-    flint = {
-        texture = 'flint_obj.png',
-        title = 'Flint',
-        replacement = {
-            pick = 'default:pick_stone',
-            sword = 'default:sword_stone',
-            axe = 'default:axe_stone',
-            shovel = 'default:shovel_stone'
+knapping_mtg = {
+    tools = {
+        pick = {
+            title = 'Pickaxe',
+            groups = { pickaxe = 1 },
+        },
+        axe = {
+            title = 'Axe',
+            groups = { axe = 1 },
+        },
+        shovel = {
+            title = 'Shovel',
+            groups = { shovel = 1 },
+        },
+        sword = {
+            title = 'Sword',
+            groups = { sword = 1 },
         }
     },
-    obsidian = {
-        texture = 'default_obsidian.png',
-        title = 'Obsidian',
-        tool_capabilities = {
-            pick = {
-                full_punch_interval = 1.1,
-                max_drop_level = 3,
-                groupcaps = {
-                    cracky = { times = { [1] = 2.2, [2] = 1.1, [3] = 0.55 }, uses = 30, maxlevel = 3 },
-                },
-                damage_groups = { fleshy = 6 },
+    materials = {
+        flint = {
+            texture = 'flint_obj.png',
+            title = 'Flint',
+            replacement = {
+                pick = 'default:pick_stone',
+                sword = 'default:sword_stone',
+                axe = 'default:axe_stone',
+                shovel = 'default:shovel_stone'
             },
-            sword = {
-                full_punch_interval = 0.9,
-                max_drop_level = 1,
-                groupcaps = {
-                    snappy = { times = { [1] = 1.95, [2] = 0.95, [3] = 0.32 }, uses = 40, maxlevel = 3 },
+            tool_capabilities = {
+                pick = core.registered_items['default:pick_stone'].tool_capabilities,
+                sword = core.registered_items['default:sword_stone'].tool_capabilities,
+                axe = core.registered_items['default:axe_stone'].tool_capabilities,
+                shovel = core.registered_items['default:shovel_stone'].tool_capabilities,
+            }
+        },
+        obsidian = {
+            texture = 'default_obsidian.png',
+            title = 'Obsidian',
+            tool_capabilities = {
+                pick = {
+                    full_punch_interval = 1.1,
+                    max_drop_level = 3,
+                    groupcaps = {
+                        cracky = { times = { [1] = 2.2, [2] = 1.1, [3] = 0.55 }, uses = 30, maxlevel = 3 },
+                    },
+                    damage_groups = { fleshy = 6 },
                 },
-                damage_groups = { fleshy = 9 },
-            },
-            axe = {
-                full_punch_interval = 1.1,
-                max_drop_level = 1,
-                groupcaps = {
-                    choppy = { times = { [1] = 2.15, [2] = 0.95, [3] = 0.55 }, uses = 30, maxlevel = 3 },
+                sword = {
+                    full_punch_interval = 0.9,
+                    max_drop_level = 1,
+                    groupcaps = {
+                        snappy = { times = { [1] = 1.95, [2] = 0.95, [3] = 0.32 }, uses = 40, maxlevel = 3 },
+                    },
+                    damage_groups = { fleshy = 9 },
                 },
-                damage_groups = { fleshy = 8 },
-            },
-            shovel = {
-                full_punch_interval = 1.2,
-                max_drop_level = 1,
-                groupcaps = {
-                    crumbly = { times = { [1] = 1.15, [2] = 0.55, [3] = 0.30 }, uses = 30, maxlevel = 3 },
+                axe = {
+                    full_punch_interval = 1.1,
+                    max_drop_level = 1,
+                    groupcaps = {
+                        choppy = { times = { [1] = 2.15, [2] = 0.95, [3] = 0.55 }, uses = 30, maxlevel = 3 },
+                    },
+                    damage_groups = { fleshy = 8 },
                 },
-                damage_groups = { fleshy = 5 },
+                shovel = {
+                    full_punch_interval = 1.2,
+                    max_drop_level = 1,
+                    groupcaps = {
+                        crumbly = { times = { [1] = 1.15, [2] = 0.55, [3] = 0.30 }, uses = 30, maxlevel = 3 },
+                    },
+                    damage_groups = { fleshy = 5 },
+                }
             }
         }
-    },
+    }
 }
 
 if core.global_exists('stoneage') then
-    materials.flint = nil
+    knapping_mtg.materials.flint = nil
 end
 
-for name, def in pairs(tools) do
-    for material, props in pairs(materials) do
+for name, def in pairs(knapping_mtg.tools) do
+    for material, props in pairs(knapping_mtg.materials) do
         local item_name = "knapping_mtg:" .. material .. "_" .. name .. "_head"
         core.register_craftitem(item_name, {
             description = "Knapped " .. props.title .. ' ' .. def.title .. " Head",
             inventory_image = material .. "_" .. name .. "_head.png",
         })
 
-        knapping.register_recipe({
-            input = "default:" .. material,
-            output = item_name,
-            recipe = knapping.default_recipes[name],
+        craftlib.register_recipe({
+            type = "knap",
+            base = {"default:" .. material},
+            output = {item_name},
+            pattern = knapping.default_recipes[name],
             texture = props.texture
         })
 
         local tool_name = ''
+        local tool_def = {
+            description = "Knapped " .. props.title .. ' ' .. def.title,
+            inventory_image = texture or material .. "_" .. name .. ".png",
+            wield_image = texture or (name == 'shovel' and (material .. "_" .. name .. ".png^[transformR90") or (material .. "_" .. name .. ".png")),
+            tool_capabilities = props.tool_capabilities[name],
+            sound = { breaks = "default_tool_breaks" },
+            groups = def.groups,
+        }
 
         if props.replacement and props.replacement[name] then
             tool_name = props.replacement[name]
-
             core.clear_craft({
                 output = props.replacement[name]
             })
-
-            core.override_item(tool_name, {
-                inventory_image = material .. "_" .. name .. ".png",
-                description = props.title .. " " .. def.title,
-                wield_image = name == 'shovel' and (material .. "_" .. name .. ".png^[transformR90") or (material .. "_" .. name .. ".png"),
-            })
+            core.override_item(tool_name, tool_def)
         else
             tool_name = 'knapping_mtg:' .. name .. '_' .. material
-
-            core.register_tool(tool_name, {
-                description = "Knapped " .. props.title .. ' ' .. def.title,
-                inventory_image = texture or material .. "_" .. name .. ".png",
-                wield_image = texture or (name == 'shovel' and (material .. "_" .. name .. ".png^[transformR90") or (material .. "_" .. name .. ".png")),
-                tool_capabilities = props.tool_capabilities[name],
-                sound = { breaks = "default_tool_breaks" },
-                groups = def.groups,
-            })
+            core.register_tool(tool_name, tool_def)
         end
 
-        core.register_craft({
-            output = tool_name,
-            type = 'shaped',
-            recipe = {
-                { item_name },
-                { 'group:stick' }
-            }
+        craftlib.register_craft({
+            type = 'craft',
+            base = {'group:stone', 'group:soil'},
+            input = { item_name },
+            tool = 'group:stick',
+            output = tool_name
         })
     end
 end
