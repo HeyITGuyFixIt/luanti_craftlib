2c6445baf6d7a5a340193552317bd1fbbd9cd137
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2026-05-14 17:18:14 -0400

    Switched from js syntax to valid lua

--- craftlib/api.lua
+++ craftlib/api.lua
@@ -1,4 +1,4 @@
-craftlib.register_craft = function (def) {
+craftlib.register_craft = function (def)
     -- Expects def to be {
     --      type = "craft"|"mix"|"knap"|"toolrepair"
     --      base = list<node|group>,
@@ -8,7 +8,7 @@ craftlib.register_craft = function (def) {
     --      output? = list<node|item|tool|group>
     -- }
     -- node, item, tool, and group are strings, optionally followed by a quantity
-    if def.base ~= nil and #def.base ~= 0 {
+    if def.base ~= nil and #def.base ~= 0 then
         for _, base in ipairs(def.base) do
             if string.match(base, "^group:") then -- base is a group
                 local group = string.match(base, "^group:([^ ]+)")
@@ -31,7 +31,11 @@ craftlib.register_craft = function (def) {
                 end
             end
         end
-    } else {
+    else
         core.log("warn", "[craftlib] Registered craft is missing a base material: "..dump(def))
-    }
-}
+    end
+end
+
+craftlib.is_crafting_activated = function(pos, node, digger)
+    --
+end
