6e37fe55d7ce291110f503990d08b2689a0ed240
Christian Sirolli <34974905+HeyITGuyFixIt@users.noreply.github.com>
2026-05-19 16:30:34 -0400

    wip

---
+++ metalworking/functions.lua
@@ -0,0 +1,20 @@
+function metalworking.remove_material(mod, material, tools, ingot)
+    if tools then
+        core.clear_craft({ output = mod .. ":axe_" .. material })
+        core.clear_craft({ output = mod .. ":pick_" .. material })
+        core.clear_craft({ output = mod .. ":shovel_" .. material })
+        core.clear_craft({ output = mod .. ":sword_" .. material })
+    end
+    if ingot then
+        local ingot_name = mod .. ":" .. material .. "_ingot"
+        local recipes = core.get_all_craft_recipes(ingot_name)
+        if recipes then
+            for _, recipe in ipairs(recipes) do
+                if recipe.method == "cooking" then
+                    core.clear_craft({ type = "cooking", recipe = recipe.items[1] })
+                    metalworking.register_sand_cast(recipe.items[1], ingot_name)
+                end
+            end
+        end
+    end
+end
--- metalworking/molds.lua
+++ metalworking/molds.lua
@@ -3,14 +3,14 @@ craftlib.register_craft({
     base = {"group:slab"},
     tool = { "group:chisel", "group:axe" },
     pattern = {
-        {1, 1, 1, 1, 1, 1, 1, 1},
-        {1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 1, 0, 0, 0, 0, 0, 1},
-        {1, 1, 1, 0, 0, 1, 1, 1},
-        {1, 1, 1, 0, 0, 1, 1, 1},
-        {1, 1, 0, 0, 0, 0, 1, 1},
-        {1, 0, 0, 0, 0, 0, 0, 1},
-        {1, 1, 1, 1, 1, 1, 1, 1}
+        {0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 0},
+        {0, 0, 1, 1, 1, 1, 1, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 0, 0, 1, 1, 0, 0, 0},
+        {0, 0, 1, 1, 1, 1, 0, 0},
+        {0, 1, 1, 1, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0}
     },
     output = { "metalworking:pattern_anvil" }
 })
@@ -18,3 +18,16 @@ craftlib.register_craft({
 core.register_node("metalworking:pattern_anvil", {
     name = "Metal Anvil Pattern",
 })
+
+metalworking.remove_material("default", "wood", true, false)
+metalworking.remove_material("default", "stone", true, false)
+metalworking.remove_material("default", "steel", true, true)
+metalworking.remove_material("default", "bronze", true, false)
+metalworking.remove_material("default", "copper", false, true)
+metalworking.remove_material("default", "gold", false, true)
+metalworking.remove_material("default", "tin", false, true)
+metalworking.remove_material("default", "mese", true, false)
+metalworking.remove_material("default", "diamond", true, false)
+if core.get_modpath("ethereal") then
+    metalworking.remove_material("ethereal", "crystal", true, false)
+end
